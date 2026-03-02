49c7fe7421dd8b0a5085465c5586a8634724fd97
aryanmalhotra0703 <aryanmalhotra.0703@gmail.com>
2026-03-02 14:10:30 +0530

    Update 4_top_paying_skills.sql

--- project_sql/4_top_paying_skills.sql
+++ project_sql/4_top_paying_skills.sql
@@ -1,3 +1,11 @@
+/*
+Answer: What are the top skills based on salary?
+- Look at the average salary associated with each skill for Data Analyst positions
+- Focuses on roles with specified salaries, regardless of location
+- Why? It reveals how different skills impact salary levels for Data Analysts and 
+    helps identify the most financially rewarding skills to acquire or improve
+*/
+
 SELECT 
     skills_dim.skills AS skill,
     ROUND(AVG(salary_year_avg),0) AS average_salary
